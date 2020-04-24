e8e818acd7d0bb1bf99b98784e27cce1a9b3ef9b
Alif <mail@ahmedalif.com>
2020-04-24 23:32:59 +0600

    Image pick, crop, edit added

--- lib/image_page.dart
+++ lib/image_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 //constants
 const buttonTextStyle = TextStyle(
   color: Colors.white,
+  fontWeight: FontWeight.w900,
 );
 
 class ImagePage extends StatefulWidget {
@@ -13,35 +16,100 @@ class ImagePage extends StatefulWidget {
 
 class _ImagePageState extends State<ImagePage> {
   io.File image;
+  io.File croppedImage;
 
-  getImageFile() async {}
+  getImageFile(ImageSource source) async {
+    var getImage = await ImagePicker.pickImage(source: source);
+    croppedImage = await ImageCropper.cropImage(
+        sourcePath: getImage.path,
+        aspectRatioPresets: io.Platform.isAndroid
+            ? [
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio16x9
+              ]
+            : [
+                CropAspectRatioPreset.original,
+                CropAspectRatioPreset.square,
+                CropAspectRatioPreset.ratio3x2,
+                CropAspectRatioPreset.ratio4x3,
+                CropAspectRatioPreset.ratio5x3,
+                CropAspectRatioPreset.ratio5x4,
+                CropAspectRatioPreset.ratio7x5,
+                CropAspectRatioPreset.ratio16x9
+              ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Image Edit',
+            toolbarColor: Colors.cyan,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Edit',
+        ));
+
+    setState(() {
+      image = croppedImage;
+      if (image != null) {
+        print("${image.lengthSync() / 1000} kB");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Insert',
+          'ImageShape App',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: image == null
-            ? Text(
-                'Insert an image',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20.0,
-                ),
-              )
-            : Image.file(
-                image,
-                width: 300,
-                height: 300,
-              ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            image != null
+                ? Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 25.0,
+                    ),
+                    child: FloatingActionButton.extended(
+                      onPressed: () {},
+                      label: Text(
+                        'Insert Shapes to the Image',
+                        style: buttonTextStyle,
+                      ),
+                      icon: Icon(
+                        Icons.add_photo_alternate,
+                        color: Colors.white,
+                      ),
+                      heroTag: UniqueKey(),
+                    ),
+                  )
+                : SizedBox(),
+            Center(
+              child: image == null
+                  ? Text(
+                      'Insert an image',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  : Image.file(
+                      image,
+                      fit: BoxFit.cover,
+                    ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -52,7 +120,9 @@ class _ImagePageState extends State<ImagePage> {
               width: 25,
             ),
             FloatingActionButton.extended(
-              onPressed: () {},
+              onPressed: () {
+                getImageFile(ImageSource.camera);
+              },
               label: Text(
                 'Open Camera',
                 style: buttonTextStyle,
@@ -67,7 +137,9 @@ class _ImagePageState extends State<ImagePage> {
               width: 15,
             ),
             FloatingActionButton.extended(
-              onPressed: () {},
+              onPressed: () {
+                getImageFile(ImageSource.gallery);
+              },
               label: Text(
                 'Open Gallery',
                 style: buttonTextStyle,
